7097772db9a851762855e5327d395d0d2674ccda
ElijahAbriol <elijahmarujahabriol@gmail.com>
2025-04-03 11:32:44 +0800

    Add files via upload
    added single array

---
+++ Abriol_SingleArray.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Midterm;
+
+/**
+ *
+ * @author User
+ */
+public class Abriol_SingleArray {
+    
+    public static void main(String[]args){
+        char letters[]= {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        
+        for (char x: letters){
+            System.out.print (x +  " , ");
+           
+    
+            
+        }
+         
+        }
+        
+    }
+    
+
